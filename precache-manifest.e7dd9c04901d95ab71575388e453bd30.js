74f75c8d4199d828ba0c6c15f255a7e84ca33a04
electreact <118991523+electreact@users.noreply.github.com>
2022-11-24 17:10:26 -0300

    Add files via upload

---
+++ precache-manifest.e7dd9c04901d95ab71575388e453bd30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6065523c323812cddbc8ef978d656b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "585eda9b37d9810b9a16",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8e2f9fad70296fb106a5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "585eda9b37d9810b9a16",
+    "url": "/static/js/2.b31e1aa3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b31e1aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2f9fad70296fb106a5",
+    "url": "/static/js/main.f3959792.chunk.js"
+  },
+  {
+    "revision": "f56fd9da806102b5ecc2",
+    "url": "/static/js/runtime-main.a3f66df3.js"
+  }
+]);
